0b99fb18749c19337351e5d5e7528ee922eb4726
Navnath Kadam <navnathkadam284@gmail.com>
2025-07-16 10:17:45 +0530

    Configured React Vite + Tailwind in React Vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className='m-10'>
+        This is App Page
+     </div>
+    </>
+  )
+}
+
+export default App
