7a0e44ee312e02e0f459f53e08c513b1841323f4
Navnath Kadam <navnathkadam284@gmail.com>
2025-07-19 17:28:16 +0530

    Added Encryption in LocalStorage for Mode

--- frontend/src/store/slices/useThemeStore.js
+++ frontend/src/store/slices/useThemeStore.js
@@ -1,8 +1,12 @@
 import { createPersistedStore } from "../store";
+import CryptoJs from 'crypto-js'
 
 const useThemeStore = createPersistedStore("theme", (set) => ({
-    mode: "light",
-    setMode: () => set((state) => ({ mode: state.mode === 'dark' ? 'light' : 'dark' }))
+    mode: CryptoJs.AES.encrypt("light",import.meta.env.VITE_ENCRYPTION_SECRET).toString(),
+    setMode: () => set((state) => {
+        const decryptedMode = CryptoJs.AES.decrypt(state.mode,import.meta.env.VITE_ENCRYPTION_SECRET).toString(CryptoJs.enc.Utf8)
+        return { mode: CryptoJs.AES.encrypt(decryptedMode === 'dark' ? 'light' : 'dark',import.meta.env.VITE_ENCRYPTION_SECRET).toString() }
+    })
 }))
 
 export { useThemeStore }
