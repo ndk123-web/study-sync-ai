66a29648d610981c5f365db2986da52ad09927bd
Navnath Kadam <navnathkadam284@gmail.com>
2025-08-03 13:09:59 +0530

    fix: if refresh then progress for that course for current user still will be the current progress of that user

---
+++ frontend/src/api/GetCurrentCourseProgressApi.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const GetCurrentCourseProgressApi = async (courseId) => {
+  try {
+    const backendResponse = await axios.post(
+      "http://localhost:5000/api/v1/courses/get-current-course-progress",
+      {
+        courseId: courseId,
+      },
+      {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    console.log("backendApiResponse: ", backendResponse);
+
+    return {
+      status: backendResponse.status,
+      data: backendResponse.data.data.progress,
+    };
+  } catch (err) {
+    console.log("Err in Getting All Courses Api: ", err.message);
+    return {
+      status: err.response?.status || 500,
+      data: err.response?.data || {},
+      message: err.message,
+    };
+  }
+};
+
+export { GetCurrentCourseProgressApi };
+
+    
